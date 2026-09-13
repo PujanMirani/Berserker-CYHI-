29b28529e4a77c1e61fa46497666729020365d7a
PujanMirani <pujanmirani2708@gmail.com>
2026-09-13 06:47:11 +0530

    Fix layout word-wrapping corrupting ASCII art

--- main.go
+++ main.go
@@ -139,7 +139,7 @@ func (m model) View() string {
 		fmt.Sprintf("%s\n%s", highlightStyle.Render("Welcome to Berserker Toolkit!"), time.Now().Format("15:04:05")),
 	)
 	
-	avatarBox := baseStyle.Width(leftWidth).Align(lipgloss.Left).Render(
+	avatarBox := baseStyle.Align(lipgloss.Left).Render(
 		titleStyle.Render(avatarAscii),
 	)
 
@@ -153,7 +153,7 @@ func (m model) View() string {
 	leftCol := lipgloss.JoinVertical(lipgloss.Left, welcomeBox, avatarBox, controlsBox)
 
 	// RIGHT COLUMN PANELS
-	logoBox := baseStyle.Width(rightWidth).Align(lipgloss.Center).Render(
+	logoBox := baseStyle.Align(lipgloss.Center).Render(
 		titleStyle.Render(logoAscii),
 	)
 
